d9e31f03ef73ef8e8149b894fb865f92f7f72e28
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-02-24 19:54:08 +0900

    support multiple repositories and arbitrary keywords
    fixes #9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,33 @@
+### Feature
+
+#### Multiple repository support
+
+It is now possible to support multiple repositories with`freebsd_repos`.
+
+### Incompatibility
+
+The following variables have been removed.
+
+* `freebsd_repos_name`
+* `freebsd_repos_url`
+* `freebsd_repos_mirror_type`
+* `freebsd_repos_mirror_signature_type`
+* `freebsd_repos_priority`
+* `freebsd_repos_disable_default_repository`
+
+Use `freebsd_repos`, which supports all ketwords described in
+`pkg.conf(5)`.
+
+`freebsd_repos_disable_default_repository` should be replaced with:
+
+```yaml
+freebsd_repos:
+  FreeBSD:
+    enabled: "false"
+    state: present
+```
+
+
 ## Release 1.1.0
 
 * 01398c3 [bugfix] validate pkg.conf(5) (#12)
--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -4,25 +4,28 @@ require 'serverspec'
 describe file('/usr/local/etc/pkg/repos/FreeBSD.conf') do
   it { should be_file }
   it { should be_mode 644 }
-  its(:content) { should match /FreeBSD: { enabled: no }/ }
+  its(:content) { should match /^FreeBSD: {\n\s+enabled: false,\n}/ }
 end
 
-describe file('/usr/local/etc/pkg/repos/reallyenglish.conf') do
-  it { should be_file }
-  it { should be_mode 644 }
-  it { should be_owned_by "root" }
-  it { should be_grouped_into "wheel" }
-  its(:content) { should match /^reallyenglish: {/ }
-  its(:content) { should match /url: "pkg\+http:\/\/10\.3\.build\.reallyenglish.com\/\$\{ABI\}",/ }
-  its(:content) { should match /mirror_type: "srv",/ }
-  its(:content) { should match /signature_type: "none",/ }
-  its(:content) { should match /enabled:\s+yes/ }
-  its(:content) { should match /priority:\s+100/ }
+[ "10.3.build", "10.1.build" ].each do |r|
+  describe file("/usr/local/etc/pkg/repos/#{ r }.conf") do
+    it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by "root" }
+    it { should be_grouped_into "wheel" }
+    its(:content) { should match /^#{ Regexp.escape(r) }: {/ }
+    its(:content) { should match /url: "pkg\+http:\/\/#{ Regexp.escape(r) }\.reallyenglish.com\/\$\{ABI\}",/ }
+    its(:content) { should match /mirror_type: "srv",/ }
+    its(:content) { should match /signature_type: "none",/ }
+    its(:content) { should match /enabled:\s+true/ }
+    its(:content) { should match /priority:\s+100/ }
+  end
 end
 
 describe command('pkg -vv') do
   its(:exit_status) { should eq 0 }
   its(:stderr) { should match(/^$/) }
-  its(:stdout) { should match(/^\s+reallyenglish:\s+{/) }
+  its(:stdout) { should match(/^\s+10\.3\.build:\s+{/) }
+  its(:stdout) { should match(/^\s+10\.1\.build:\s+{/) }
   its(:stdout) { should_not match(/^\s+FreeBSD:\s+{/) }
 end

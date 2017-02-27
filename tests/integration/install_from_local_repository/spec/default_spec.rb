475938968914e06186543fbc634a086ac1f565ea
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-02-27 15:42:08 +0900

    create a method to return http_proxy_url
    when the proxy is running

--- tests/integration/install_from_local_repository/spec/default_spec.rb
+++ tests/integration/install_from_local_repository/spec/default_spec.rb
@@ -10,7 +10,7 @@ context 'after provisioning finished' do
   describe server(:server1) do
 
     it "is able to install logstash5" do
-      result = current_server.ssh_exec("sudo env ASSUME_ALWAYS_YES=yes HTTP_PROXY=#{ http_proxy_url if proxy_running? } pkg install logstash5 && echo LOGSTASH_INSTALLED")
+      result = current_server.ssh_exec("sudo env ASSUME_ALWAYS_YES=yes HTTP_PROXY=#{ http_proxy } pkg install logstash5 && echo LOGSTASH_INSTALLED")
       expect(result).to match(/^(?:\[\d+\/\d+\] Installing logstash|The most recent version of packages are already installed)/)
       expect(result).to match(/LOGSTASH_INSTALLED/)
     end
